a34446fb136b4373e68ee0bf981f0a73c68cc86c
caihaokun <1661276674@qq.com>
2025-06-18 17:01:49 +0800

    added utils folder

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import numpy as np
+import sys
 from PackageTest import *
-
-
-
+from Utils.LLMUtils import *
 
 
 
 
 if __name__ == "__main__":
     print("Initial\n")
+    test()
     PTestQt()
